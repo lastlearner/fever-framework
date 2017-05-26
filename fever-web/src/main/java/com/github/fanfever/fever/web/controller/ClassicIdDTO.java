5a157f150256496cdb8d0afbbc13f6f7e2a7c2c8
fanfever <fanfever@fanfeverdeMacBook-Pro.local>
2017-05-26 20:26:24 +0800

    fixed page

--- fever-web/src/main/java/com/github/fanfever/fever/web/controller/ClassicIdDTO.java
+++ fever-web/src/main/java/com/github/fanfever/fever/web/controller/ClassicIdDTO.java
@@ -7,41 +7,50 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 /**
  * Created by fanfever on 2017/4/10.
  * Email fanfeveryahoo@gmail.com
  * Url https://github.com/fanfever
  */
 @Slf4j
-@JsonIgnoreProperties(ignoreUnknown = true)
-public
+@JsonIgnoreProperties(ignoreUnknown = true, value = "clazz")
 @Data
 @NoArgsConstructor
-class ClassicIdDTO<T extends BaseModel> implements Converter<ClassicIdDTO, T> {
+public class ClassicIdDTO<T extends BaseModel> implements Converter<ClassicIdDTO, T> {
 
     private Integer id;
 
     private Class<T> clazz;
 
-    public ClassicIdDTO(Class<T> clazz){
+    public ClassicIdDTO(Class<T> clazz) {
         this.clazz = clazz;
     }
 
     @Override
     public T convert(final ClassicIdDTO s) {
-        if(null == s){
+        if (null == s) {
             return null;
         }
         T target = null;
         try {
-            target = null == clazz ? (T)new BaseModel() : clazz.newInstance();
-            if(null != s.getId()){
+            target = null == clazz ? (T) new BaseModel() : clazz.newInstance();
+            if (null != s.getId()) {
                 target.setId(s.getId());
             }
         } catch (InstantiationException | IllegalAccessException e) {
             log.error("exception:{}", e);
         }
         return target;
+    }
+
+    public static <T> T convert(ClassicIdDTO s, Class<T> targetClass) {
+        return (T) new ClassicIdDTO(targetClass).convert(s);
+    }
+
+    public static List convert(List<ClassicIdDTO> s) {
+        return new ClassicIdDTO().convertToList(s);
     }
 
 }
