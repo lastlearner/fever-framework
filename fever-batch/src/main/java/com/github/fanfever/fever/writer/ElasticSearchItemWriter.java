479851589d32bd93530368eeacaf7451a9409d0e
fanfever <fanfever@fanfeverdeMacBook-Pro.local>
2017-06-22 06:42:12 +0800

    add operator unit test

--- fever-batch/src/main/java/com/github/fanfever/fever/writer/ElasticSearchItemWriter.java
+++ fever-batch/src/main/java/com/github/fanfever/fever/writer/ElasticSearchItemWriter.java
@@ -1,32 +1,30 @@
 package com.github.fanfever.fever.writer;
 
-import com.github.fanfever.fever.command.DocumentCommand;
 import com.github.fanfever.fever.command.enums.DocumentCommandType;
 import com.github.fanfever.fever.command.request.DocumentCommandRequest;
 import com.github.fanfever.fever.dto.BaseDocument;
 import com.google.common.collect.Lists;
-import lombok.AllArgsConstructor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Component;
 
-import javax.print.Doc;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Created by fanfever on 2017/6/22.
  * Email fanfeveryahoo@gmail.com
  * Url https://github.com/fanfever
  */
-@Component
-@AllArgsConstructor
-public class ElasticSearchItemWriter implements ItemWriter<BaseDocument> {
+public class ElasticSearchItemWriter<T> implements ItemWriter<T>, InitializingBean {
 
-    private final DocumentCommand documentCommand;
+    private String INDEX;
+    private String TYPE;
 
     @Override
-    public void write(List<? extends BaseDocument> itemList) throws Exception {
-        documentCommand.execute(itemList.stream().map(i -> DocumentCommandRequest.of(DocumentCommandType.UPSERT,i.getIndex(),i.getType(),i.getId()).setDocument(i)).collect(Collectors.toList()));
+    public void afterPropertiesSet() throws Exception {
+    }
+
+    @Override
+    public void write(List<? extends T> itemList) throws Exception {
+//        itemList.stream().map(i -> DocumentCommandRequest.of(DocumentCommandType.UPSERT,INDEX,TYPE,i.getId()));
     }
 }
