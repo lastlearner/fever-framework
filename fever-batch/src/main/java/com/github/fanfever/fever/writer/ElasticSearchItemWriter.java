4a2dfa9ea1da9c16f179189ad1d8e3ff76d0fda9
fanfever <fanfever@fanfeverdeMacBook-Pro.local>
2017-06-22 06:54:08 +0800

    add operator unit test

--- fever-batch/src/main/java/com/github/fanfever/fever/writer/ElasticSearchItemWriter.java
+++ fever-batch/src/main/java/com/github/fanfever/fever/writer/ElasticSearchItemWriter.java
@@ -1,26 +1,32 @@
 package com.github.fanfever.fever.writer;
 
+import com.github.fanfever.fever.command.DocumentCommand;
+import com.github.fanfever.fever.command.enums.DocumentCommandType;
+import com.github.fanfever.fever.command.request.BaseDocument;
+import com.github.fanfever.fever.command.request.DocumentCommandRequest;
+import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.ReflectionUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by fanfever on 2017/6/22.
  * Email fanfeveryahoo@gmail.com
  * Url https://github.com/fanfever
  */
-public class ElasticSearchItemWriter<T> implements ItemWriter<T>, InitializingBean {
+@AllArgsConstructor
+public class ElasticSearchItemWriter implements ItemWriter<BaseDocument> {
 
-    private String INDEX;
-    private String TYPE;
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-    }
+    private final String INDEX;
+    private final String TYPE;
+    private final DocumentCommand documentCommand;
 
     @Override
-    public void write(List<? extends T> itemList) throws Exception {
-//        itemList.stream().map(i -> DocumentCommandRequest.of(DocumentCommandType.UPSERT,INDEX,TYPE,i.getId()));
+    public void write(List<? extends BaseDocument> itemList) throws Exception {
+        documentCommand.execute(itemList.stream().map(i -> DocumentCommandRequest.of(DocumentCommandType.UPSERT,INDEX,TYPE, i.getId())).collect(Collectors.toList()));
     }
 }
---
+++ fever-elasticsearch/src/main/java/com/github/fanfever/fever/command/request/BaseDocument.java
@@ -0,0 +1,13 @@
+package com.github.fanfever.fever.command.request;
+
+import lombok.Data;
+
+/**
+ * Created by fanfever on 2017/6/22.
+ * Email fanfeveryahoo@gmail.com
+ * Url https://github.com/fanfever
+ */
+@Data
+public abstract class BaseDocument {
+    private Integer id;
+}
