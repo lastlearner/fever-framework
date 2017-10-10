8319836cfdc80710a9609bbe19858a04275de9c9
fanfever <fanfeveryahoo@gmail.com>
2017-10-10 16:27:14 +0800

    add baseDocument

--- fever-elasticsearch/src/main/java/com/github/fanfever/fever/command/request/BaseDocument.java
+++ fever-elasticsearch/src/main/java/com/github/fanfever/fever/command/request/BaseDocument.java
@@ -1,9 +1,13 @@
 package com.github.fanfever.fever.command.request;
 
+import lombok.Data;
+
 /**
  * Created by fanfever on 2017/10/10.
  * Email fanfeveryahoo@gmail.com
  * Url https://github.com/fanfever
  */
-public class BaseDocument {
+@Data
+public abstract class BaseDocument {
+    private Integer id;
 }
